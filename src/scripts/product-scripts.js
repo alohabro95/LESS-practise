448768a4f690bbd3b2301bde17cea286083621e2
Gennadiy Saranchuk <alooha.broo@gmail.com>
2024-06-27 17:21:28 +0500

    feat: add header to every page

--- src/scripts/product-scripts.js
+++ src/scripts/product-scripts.js
@@ -64,3 +64,21 @@ document.addEventListener("DOMContentLoaded", function () {
   minusButton.addEventListener("click", decrementNumber);
   plusButton.addEventListener("click", incrementNumber);
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("layout/header.html")
+    .then((response) => response.text())
+    .then((data) => {
+      const tempDiv = document.createElement("div");
+      tempDiv.innerHTML = data;
+
+      // Вставка стилей
+      const styles = tempDiv.querySelectorAll('link[rel="stylesheet"]');
+      styles.forEach((style) => document.head.appendChild(style));
+
+      // Вставка header
+      document.getElementById("header-placeholder").innerHTML =
+        tempDiv.querySelector(".header").outerHTML;
+    })
+    .catch((error) => console.error("Error loading header:", error));
+});
