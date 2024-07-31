0357a6f4367d405b40f44175f604a86a9cc6d52c
Gennadiy Saranchuk <alooha.broo@gmail.com>
2024-07-31 18:06:17 +0500

    refactor: remove products array

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,114 +1,23 @@
-var slickSettings = {
-  dots: true,
-  speed: 500,
-  slidesToShow: 2,
-  slidesToScroll: 1,
-  prevArrow:
-    "<button class='prev slider__btn'><img src='images/products__images/right.svg'></button>",
-  nextArrow:
-    "<button class='next slider__btn'><img src='images/products__images/right.svg'></button>",
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        centerMode: false,
-        variableWidth: false,
-      },
-    },
-  ],
-};
-$(".rooms__slider").slick(slickSettings);
+import { scrollTo } from "./lib.js";
+import { loadLayout } from "./layout.js";
+import { updateCartCount, addToCart } from "./lib.js";
 
-var scrollBtn = document.getElementById("down");
-var scrollElement = document.getElementById("section-products");
+document.addEventListener("DOMContentLoaded", function () {
+  try {
+    loadLayout();
+    setTimeout(() => {
+      updateCartCount();
+    }, 100);
+    displayProducts(startIndex, productsPerPage);
+  } catch (error) {
+    console.error("Error loading layout:", error);
+  }
+});
 
-function scroll() {
-  scrollElement.scrollIntoView({ block: "center", behavior: "smooth" });
-}
-
-scrollBtn.addEventListener("click", scroll);
-
-const products = [
-  {
-    name: "Syltherine",
-    description: "Stylish cafe chair",
-    price: "$ 250",
-    discount: "$ 35",
-    image: "/images/products__images/Syltherine.png",
-    type: "small",
-  },
-  {
-    name: "Leviosa",
-    description: "Stylish cafe chair",
-    price: "$ 100",
-    discount: "",
-    image: "/images/products__images/Leviosa.png",
-    type: "small",
-  },
-  {
-    name: "Lolito",
-    description: "Luxury big sofa",
-    price: "$ 700",
-    discount: "$ 140",
-    image: "/images/products__images/Lolito.png",
-    type: "large",
-  },
-  {
-    name: "Respira",
-    description: "Outdoor bar table and stool",
-    price: "$ 500",
-    discount: "",
-    image: "/images/products__images/Respira.png",
-    type: "large",
-  },
-  {
-    name: "Grifo",
-    description: "Night lamp",
-    price: "$ 50",
-    discount: "",
-    image: "/images/products__images/Grifo.png",
-    type: "other",
-  },
-  {
-    name: "Muggo",
-    description: "Small mug",
-    price: "$ 350",
-    discount: "",
-    image: "/images/products__images/Muggo.png",
-    type: "large",
-  },
-  {
-    name: "Pingky",
-    description: "Cute bed set",
-    price: "$ 700",
-    discount: "$ 140",
-    image: "/images/products__images/Pingky.png",
-    type: "large",
-  },
-  {
-    name: "Potty",
-    description: "Minimalist flower pot",
-    price: "$ 500",
-    discount: "",
-    image: "/images/products__images/Lolito.png",
-    type: "large",
-  },
-  {
-    name: "Potty",
-    description: "Minimalist flower pot",
-    price: "$ 500",
-    discount: "",
-    image: "/images/products__images/Lolito.png",
-  },
-];
-
-const localProducts = localStorage.setItem(
-  "localProducts",
-  JSON.stringify(products)
-);
-const showMoreBtn = document.getElementById("products__btn");
 const productsPerPage = 4;
 let startIndex = 0;
+
+const products = JSON.parse(localStorage.getItem("localProducts")) || [];
 
 function displayProducts(startIndex, count) {
   const container = document.getElementById("product-container");
@@ -129,7 +38,7 @@ function displayProducts(startIndex, count) {
       <div class="hover-container">
         <div class="buttons">
           <div class="buttons__first">
-            <button class="add" onclick="addToCart(${i})">Add to cart</button>
+            <button class="add" data-index="${i}">Add to cart</button>
           </div>
           <div class="buttons__second">
             <button class="el-btn"><img class="logo-btn" src="/images/products__images/img__buttons/shire.svg" alt=""><span class="text-btn">Share</span></button>
@@ -142,34 +51,47 @@ function displayProducts(startIndex, count) {
     container.appendChild(productElement);
   }
 
-  if (startIndex + count < products.length) {
-    showMoreBtn.style.display = "block";
-  } else {
-    showMoreBtn.style.display = "none";
-  }
+  const addButtons = container.querySelectorAll(".add");
+  addButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const productIndex = parseInt(button.getAttribute("data-index"), 10);
+      addToCart(productIndex);
+    });
+  });
+
+  showMoreBtn.style.display =
+    startIndex + count < products.length ? "block" : "none";
 }
+
+const showMoreBtn = document.getElementById("products__btn");
 
 showMoreBtn.addEventListener("click", function () {
   startIndex += productsPerPage;
   displayProducts(startIndex, productsPerPage);
 });
 
-displayProducts(startIndex, productsPerPage);
+let scrollBtn = document.getElementById("down");
+scrollBtn.addEventListener("click", () => {
+  scrollTo("section-products");
+});
 
-function addToCart(productIndex) {
-  let cart = JSON.parse(localStorage.getItem("cart")) || [];
-  cart.push(products[productIndex]);
-  localStorage.setItem("cart", JSON.stringify(cart));
-  updateCartCount();
-}
-
-function updateCartCount() {
-  let cart = JSON.parse(localStorage.getItem("cart")) || [];
-  const cartCountElement = document.getElementById("cart-count");
-  cartCountElement.textContent = cart.length;
-  localStorage.setItem("cartCount", cart.length);
-}
-
-window.onload = function () {
-  updateCartCount();
+var slickSettings = {
+  dots: true,
+  speed: 500,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  prevArrow:
+    "<button class='prev slider__btn'><img src='/src/assets/images/products__images/right.svg'></button>",
+  nextArrow:
+    "<button class='next slider__btn'><img src='/src/assets/images/products__images/right.svg'></button>",
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        centerMode: false,
+        variableWidth: false,
+      },
+    },
+  ],
 };
+$(".rooms__slider").slick(slickSettings);
