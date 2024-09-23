9af8fe08911fa976dd6e7de2f50db48da6287e80
Gennadiy Saranchuk <alooha.broo@gmail.com>
2024-09-23 15:37:00 +0500

    change cart logic

--- src/scripts/basket-scripts.js
+++ src/scripts/basket-scripts.js
@@ -1,5 +1,6 @@
 import { appState } from "./state.js";
 import { loadLayout } from "./layout.js";
+
 document.addEventListener("DOMContentLoaded", function () {
   try {
     loadLayout()
@@ -16,60 +17,85 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function displayCart() {
-  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+  const cart = appState.getCart();
   const cartContainer = document.querySelector(".grocery-list__roster");
   cartContainer.innerHTML = "";
 
   cart.forEach((product, index) => {
+    const subTotalPrice = product.price * product.quantity;
     const productElement = document.createElement("div");
     productElement.classList.add("product");
     productElement.innerHTML = `
         <div class="first-img">
-          <img class="product__image" src="${product.image}" alt="${product.name}">
+          <img class="product__image" src="${product.image}" alt="${
+      product.name
+    }">
         </div>
         <div>
-          <span class="product__name product__name--grey el">${product.name}</span>
+          <span class="product__name product__name--grey el">${
+            product.name
+          }</span>
         </div>
         <div>
-          <span class="product__price product__name--grey el" id="product-price-${index}">${product.price}</span>
+          <span class="product__price product__name--grey el" id="product-price-${
+            product.id
+          }">
+            ${product.currency}${product.price}
+          </span>
         </div>
         <div class="quantity">
           <div class="in-quantity">
-            <input type="number" class="product__quantity-input el" id="product-quantity-${index}" value="1" min="1">
+            <input type="number" class="product__quantity-input el" id="product-quantity-${
+              product.id
+            }" value="${product.quantity}" min="1">
           </div>
         </div>
         <div>
-          <span class="product__subtotal el" id="product-subtotal-${index}">${product.price}</span>
+          <span class="product__subtotal el" id="product-subtotal-${
+            product.id
+          }">
+            ${product.currency}${subTotalPrice.toLocaleString("en-US", {
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 0,
+    })}
+          </span>
         </div>
         <div class="last-img">
-          <img class="product-bin" src="/src/assets/images/basket__images/Vector.png" alt="" onclick="removeFromCart(${index})">
+          <img class="product-bin" src="/src/assets/images/basket__images/Vector.png" alt="" onclick="removeFromCart(${
+            product.id
+          })">
         </div>
       `;
     cartContainer.appendChild(productElement);
-  });
 
-  const quantityInputs = document.querySelectorAll(".product__quantity-input");
-  quantityInputs.forEach((input, index) => {
-    input.addEventListener("change", () => {
-      updateSubtotal(index);
+    const quantityInput = document.getElementById(
+      `product-quantity-${product.id}`
+    );
+    quantityInput.addEventListener("change", () => {
+      updateSubtotal(product.id);
     });
   });
 
   updateTotal();
 }
 
-function removeFromCart(productIndex) {
+function removeFromCart(productId) {
   let cart = appState.getState().cart || [];
-  cart.splice(productIndex, 1);
+  cart = cart.filter((item) => item.id !== productId);
   appState.setCart(cart);
+  localStorage.removeItem(`quantity-${productId}`);
   displayCart();
   updateTotal();
 }
 
-function updateSubtotal(index) {
-  const quantityInput = document.getElementById(`product-quantity-${index}`);
-  const priceElement = document.getElementById(`product-price-${index}`);
-  const subtotalElement = document.getElementById(`product-subtotal-${index}`);
+function updateSubtotal(productId) {
+  const priceElement = document.getElementById(`product-price-${productId}`);
+  const quantityInput = document.getElementById(
+    `product-quantity-${productId}`
+  );
+  const subtotalElement = document.getElementById(
+    `product-subtotal-${productId}`
+  );
 
   let quantity = parseInt(quantityInput.value);
   const price = parseFloat(
@@ -81,34 +107,30 @@ function updateSubtotal(index) {
     quantityInput.value = quantity;
   }
 
-  const totalPrice = price * quantity;
+  const cart = appState.getCart();
+  const updatedCart = cart.map((item) => {
+    if (item.id === productId) {
+      item.quantity = quantity;
+      return item;
+    }
 
-  subtotalElement.textContent = `$ ${totalPrice.toLocaleString("en-US", {
-    minimumFractionDigits: 0,
-    maximumFractionDigits: 0,
-  })}`;
+    return item;
+  });
+  appState.setCart(updatedCart);
+
+  const totalPrice = price * quantity;
+  if (subtotalElement) {
+    subtotalElement.textContent = `$ ${totalPrice}`;
+  }
+
   updateTotal();
 }
 
 function updateTotal() {
-  let total = 0;
-  const subtotalElements = document.querySelectorAll(
-    "[id^='product-subtotal-']"
-  );
-
-  subtotalElements.forEach((subtotalElement) => {
-    const subtotal = parseFloat(
-      subtotalElement.textContent.replace(/[^\d.-]/g, "").replace(",", ".")
-    );
-    total += isNaN(subtotal) ? 0 : subtotal;
-  });
-
-  const subTotalElement = document.getElementById("subtotal");
-  if (subTotalElement) {
-    subTotalElement.textContent = `$ ${total.toLocaleString("en-US", {
-      minimumFractionDigits: 0,
-      maximumFractionDigits: 0,
-    })}`;
+  const total = appState.getCartTotal();
+  const totalElement = document.getElementById("total");
+  if (totalElement) {
+    totalElement.textContent = `$ ${total}`;
   }
 }
 
