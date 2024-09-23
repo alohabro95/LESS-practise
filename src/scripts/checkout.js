24e843d34e1afed01ac032778753b8e6ae750bc0
Gennadiy Saranchuk <alooha.broo@gmail.com>
2024-09-23 15:37:51 +0500

    feat: change checkout logic

--- src/scripts/checkout.js
+++ src/scripts/checkout.js
@@ -1,10 +1,12 @@
 import { loadLayout } from "./layout.js";
 import { appState } from "./state.js";
+
 document.addEventListener("DOMContentLoaded", function () {
   try {
     loadLayout()
       .then(() => {
         appState.updateCartCount();
+        displayCart();
       })
       .catch((error) => {
         console.error("Error loading layout:", error);
@@ -14,39 +16,36 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 });
 
-const products = JSON.parse(localStorage.getItem("localProducts")) || [];
-const container = document.getElementById("table");
-const cart = appState.getCart();
+function displayCart() {
+  const products = appState.getProducts();
+  const cart = appState.getCart();
+  const container = document.getElementById("table");
+
+  cart.forEach((product) => {
+    const productElement = createProductCheckout(product);
+    container.appendChild(productElement);
+  });
+
+  const total = appState.getCartTotal();
+
+  const totalElement = document.createElement("div");
+  totalElement.classList.add("table__row");
+  totalElement.innerHTML = `
+    <span class="table__cell">Total</span>
+    <span class="table__cell table__cell--bold table__cell--gold">${total.toLocaleString()} $</span>
+  `;
+
+  container.appendChild(totalElement);
+}
 
 function createProductCheckout(product) {
   const productElement = document.createElement("div");
   productElement.classList.add("table__row");
+  const subTotal = product.price * product.quantity;
   productElement.innerHTML = `
-      <span class="table__cell table__cell--grey">${product.name}</span>
-      <span class="table__cell table__cell--light">${product.price}</span>
+    <span class="table__cell table__cell--grey product-name">${product.name}</span>
+    <span class="table__cell table__cell--grey product-quantity">${product.quantity}</span>
+    <span class="table__cell table__cell--light product-subtotal">${subTotal} $</span>
   `;
   return productElement;
 }
-
-cart.forEach((cartItem) => {
-  const product = products.find((p) => p.id === cartItem.id);
-
-  if (product) {
-    const productElement = createProductCheckout(product);
-    container.appendChild(productElement);
-  }
-});
-
-const total = cart.reduce((sum, cartItem) => {
-  const product = products.find((p) => p.id === cartItem.id);
-  return sum + (product ? product.price : 0);
-}, 0);
-
-const totalElement = document.createElement("div");
-totalElement.classList.add("table__row");
-totalElement.innerHTML = `
-  <span class="table__cell">Total</span>
-  <span class="table__cell table__cell--bold table__cell--gold">Rs. ${total.toLocaleString()}</span>
-`;
-
-container.appendChild(totalElement);
